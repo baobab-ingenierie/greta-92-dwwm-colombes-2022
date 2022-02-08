b1061145dec3a737d80918b1abe31a556775b3b3
lesly-lodin <lesly.lodin@baobab-ingenierie.fr>
2022-02-08 19:39:02 +0100

    CP7 Day 13

--- cp7/class/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once 'database.class.php';
-
-echo preg_match(Database::REGEX_HOST, 'localhost');
-exit;
-
-$bdd = new Database('mysql', 'localhost', 3306, 'colombes', 'root', 'root');
-$data = $bdd->getData('SELECT * FROM user');
-var_dump($data);
